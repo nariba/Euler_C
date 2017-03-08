d230f220b501079223a0634a2f42b104e5cc6356
nariba <2IE15092R@s.kyushu-u.ac.jp>
2017-03-08 22:37:09 +0900

    Change algorithm

--- Problem0003.c
+++ Problem0003.c
@@ -16,13 +16,18 @@ int prime(long num) {
 
 int main(int argc, char const *argv[]) {
   long n=atol(argv[1]);
-  long i=sqrt(n);
-  while (1) {
-    if (n % i == 0 && prime(i) == 1) {
-      printf("%ld\n", i);
-      break;
+  long i = n / 2;
+  i = 2;
+  while (n >= i) {
+    if (prime(i) && n % i == 0) {
+      n = n / i;
     }
-    --i;
+    else {
+      ++i;
+    }
+    if (i % 10000000 == 0)
+      printf("%ld\n", i);
   }
+  printf("%ld\n", i);
   return 0;
 }
